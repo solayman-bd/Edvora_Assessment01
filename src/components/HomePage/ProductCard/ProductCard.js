eb5a31543a5fb4736e9d9fb43a90ebfe822c9285
Md. Solayman <mdsolaymannuhin@gmail.com>
2021-05-21 05:35:19 +0600

    filtering modification

--- src/components/HomePage/ProductCard/ProductCard.js
+++ src/components/HomePage/ProductCard/ProductCard.js
@@ -5,17 +5,11 @@ const ProductCard = ({ productDetail }) => {
 
   return (
     <div>
-      {name ? (
-        <div>
-          <h2>{name}</h2>
-          <h4>{brand}</h4>
-          <h3>${price}</h3>
-          <h5>City: {location.city}</h5>
-          <h5>Date: {date}</h5>
-        </div>
-      ) : (
-        <h1>No Data Available</h1>
-      )}
+      <h4>{brand}</h4>
+      <h3>${price}</h3>
+      <h5>City: {location.city}</h5>
+      <h5>State: {location.state}</h5>
+      <h5>Date: {date}</h5>
     </div>
   );
 };
